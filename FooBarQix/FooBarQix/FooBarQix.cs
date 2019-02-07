063298305851df3b421bfddbe9bbc67ff4fe3606
becharabechara <becharabechara95@hotmail.com>
2019-02-07 10:51:04 +0100

    Add files via upload

---
+++ FooBarQix/FooBarQix/FooBarQix.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace FooBarQix
+{
+    public class FooBarQix
+    {
+        private static readonly Dictionary<int, string> DictFooBarQix = new Dictionary<int, string>
+        {
+            [0] =  "*",
+            [3] = "Foo",
+            [5] = "Bar",
+            [7] = "Qix"
+        };
+        public static string Analyze(int input)
+        {
+            var res = string.Empty;
+            res += ResultDivision(input);
+            res = input.ToString().Aggregate(res,(current,t)=> current + ResultContains(Int32.Parse(t.ToString())));
+            if (Regex.IsMatch(res, @"^[\*]*\*")) res = input.ToString().Replace('0', '*');
+            return res == string.Empty? input.ToString():res;
+        }
+
+        private static string ResultDivision(int numb)
+        {
+            var result = string.Empty;
+            foreach (var val in DictFooBarQix )
+            {
+                if (val.Key != 0)
+                {
+                    if (numb % val.Key == 0) result += val.Value;
+                }
+            }
+            return result;
+        }
+
+        private static string ResultContains(int numb)
+        {
+            var result = string.Empty;
+            if (DictFooBarQix.ContainsKey(numb)) result += DictFooBarQix[numb];
+            return result;
+        }
+    }
+}
---
+++ FooBarQix/FooBarQix/FooBarQixTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+
+namespace FooBarQix
+{
+    public class FooBarQixTest
+    {
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(3, "FooFoo")]
+        [InlineData(5, "BarBar")]
+        [InlineData(6, "Foo")]
+        [InlineData(7, "QixQix")]
+        [InlineData(9, "Foo")]
+        [InlineData(10, "Bar*")]
+        [InlineData(13, "Foo")]
+        [InlineData(15, "FooBarBar")]
+        [InlineData(21, "FooQix")]
+        [InlineData(33, "FooFooFoo")]
+        [InlineData(51, "FooBar")]
+        [InlineData(53, "BarFoo")]
+        [InlineData(101, "1*1")]
+        [InlineData(303, "FooFoo*Foo")]
+        [InlineData(105, "FooBarQix*Bar")]
+        [InlineData(10101, "FooQix**")]
+        public void DigitCheck(int input, string expected)
+        {
+            var res = FooBarQix.Analyze(input);
+            Assert.Equal(expected, res);
+        }
+    }
+}
