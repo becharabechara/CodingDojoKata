a0608e554a7995499d58e79d7c5007cba30f3fa2
Bechara Bechara <bechara.bechara@viseo.com>
2019-02-08 10:06:37 +0100

    Three Of A Kind

--- PokerHands/PokerHands/PokerHandsManager.cs
+++ PokerHands/PokerHands/PokerHandsManager.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 
 namespace PokerHands
 {
-    public enum PointsGained: long
+    public enum PointsGained : long
     {
         StraightFlush = 1475789056, //14^8
         FourOfAKind = 105413504,//14^7
         FullHouse = 7529536,//14^6
         Flush = 537824,//14^5
-        Straight =  38146,//14^4
+        Straight = 38146,//14^4
         ThreeOfAKind = 2744,//14^3
         TwoPairs = 196,//14^2
         Pair = 14,//14^1
@@ -24,8 +24,8 @@ namespace PokerHands
             var ret = string.Empty;
             var player1Hand = string.Empty;
             var player2Hand = string.Empty;
-            long point1 = EvaluateDeck(Transform(player1),out player1Hand);
-            long point2 = EvaluateDeck(Transform(player2),out player2Hand);
+            long point1 = EvaluateDeck(Transform(player1), out player1Hand);
+            long point2 = EvaluateDeck(Transform(player2), out player2Hand);
             if (point1 > point2) ret = "Black wins. - with " + player1Hand;
             else if (point1 == point2) ret = "Tie.";
             else ret = "White wins. - with " + player2Hand;
@@ -35,7 +35,7 @@ namespace PokerHands
         private static List<Card> Transform(List<string> player)
         {
             List<Card> cards = new List<Card>();
-            foreach(var str in player)
+            foreach (var str in player)
             {
                 Card c = new Card();
                 c.Value = str[0];
@@ -52,28 +52,42 @@ namespace PokerHands
             Card key = new Card();
             if (IsStraight(deck) && IsFlush(deck))
             {
-                playerHand = "Straight Flush : " + deck.Max().Value + deck[0].Suit;
-                res = (long) PointsGained.StraightFlush * Int32.Parse(deck.Max().Value.ToString());
+                playerHand = "Straight Flush : " + deck.Max().Value.ToString() + deck[0].Suit;
+                res = (long)PointsGained.StraightFlush * deck.Max().getValue();
             }
             else if (IsFourOfAKind(deck, out key))
             {
-                playerHand = "Four Of A Kind : " + key.Value;
-                res = (long)PointsGained.FourOfAKind * key.Value;
+                playerHand = "Four Of A Kind : " + key.Value.ToString();
+                res = (long)PointsGained.FourOfAKind * key.getValue();
             }
             else if (IsFullHouse(deck, out key))
             {
-                playerHand = "Full House : " + key.Value;
-               res = (long)PointsGained.FullHouse * key.Value;
+                playerHand = "Full House : " + key.Value.ToString();
+                res = (long)PointsGained.FullHouse * key.getValue();
             }
-
+            else if (IsFlush(deck))
+            {
+                playerHand = "Flush : " + deck.Max().Value.ToString() + deck[0].Suit;
+                res = (long)PointsGained.Flush * deck.Max().getValue();
+            }
+            else if (IsStraight(deck))
+            {
+                playerHand = "Straight : " + deck.Max().Value.ToString() + deck[0].Suit;
+                res = (long)PointsGained.Straight * deck.Max().getValue();
+            }
+            else if (IsThreeOfAKind(deck,out key))
+            {
+                playerHand = "Three Of A Kind : " + key.Value.ToString();
+                res = (long)PointsGained.ThreeOfAKind * key.getValue();
+            }
             return res;
         }
 
         private static bool IsStraight(List<Card> deck)
         {
             deck.Sort();
-            for (int i = 0; i < deck.Count-1; i++)
-                if (deck[i].Value != deck[i+1].Value - 1)
+            for (int i = 0; i < deck.Count - 1; i++)
+                if (deck[i].Value != deck[i + 1].Value - 1)
                     return false;
             return true;
         }
@@ -122,6 +136,29 @@ namespace PokerHands
 
             return (a == 3 && b == 2) ||
                    (a == 2 && b == 3);
+        }
+
+        private static bool IsThreeOfAKind(List<Card> deck, out Card key)
+        {
+            key = new Card();
+            var distinct = deck.Distinct().ToList();
+            if (distinct.Count() > 3) return false;
+            var a = 0;
+            var b = 0;
+            var c = 0;
+            foreach (var v in deck)
+                if (v == distinct[0])
+                    a++;
+                else if (v == distinct[1])
+                    b++;
+                else
+                    c++;
+
+            key = (a == 3 && b == 1 && c == 1) ? distinct[0]
+                : (a == 1 && b == 3 && c == 1) ? distinct[1]
+                : distinct[2];
+
+            return ((a == 3 && b == 1 && c == 1) || (a == 1 && b == 3 && c == 1) || (a == 1 && b == 1 && c == 3));
         }
 
     }
--- PokerHands/PokerHands/PokerHandsTest.cs
+++ PokerHands/PokerHands/PokerHandsTest.cs
@@ -8,21 +8,21 @@ namespace PokerHands
     public class PokerHandsTest
     {
         [Theory]
-        [MemberData(nameof(GetData),parameters:2)]
+        [MemberData(nameof(GetData))]
         public void GeneralTests(List<string> blackPlayerList,List<string> whitePlayerList,string expectedWinnerWithDetails)
         {
             var res = PokerHandsManager.Compute(blackPlayerList, whitePlayerList);
             Assert.Equal(expectedWinnerWithDetails, res);
         }
 
-        public static IEnumerable<object[]> GetData(int number)
+        public static IEnumerable<object[]> GetData()
         {
             var allData = new List<object[]>
             {
                 new object[] {new List<string>{ "2C", "3H", "4S", "8C", "AH"},new List<string>{ "AH", "AD", "AS", "AC", "5D" },"White wins. - with Four Of A Kind : A"},
                 new object[] {new List<string>{"2H","3H","5H","4H","6H"},new List<string>{"2C","2C","2C","8H","8H"},"Black wins. - with Straight Flush : 6H"}
             };
-            return allData.Take(number);
+           return allData;
         }
     }
 }
