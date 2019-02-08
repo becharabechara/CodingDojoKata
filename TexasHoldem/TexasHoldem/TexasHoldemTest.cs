59636c0d5f0f3bf3c317635b8af7cb5d96aba155
Bechara Bechara <bechara.bechara@viseo.com>
2019-02-08 17:32:55 +0100

    Final Tests

--- TexasHoldem/TexasHoldem/TexasHoldemTest.cs
+++ TexasHoldem/TexasHoldem/TexasHoldemTest.cs
@@ -7,7 +7,6 @@ namespace TexasHoldem
     public class TexasHoldemTest
     {
         [Theory]
-
         [InlineData("KC 9S KS KD 9D 3C 6D\n"
                     +"9C AH KS KD 9D 3C 6D\n"
                     +"AC QC KS KD 9D 3C\n"
@@ -18,6 +17,18 @@ namespace TexasHoldem
                     + "3C 6D 9C 9D KD KS AH Two Pairs : Pair1 of K, Pair2 of 9, A\n"
                     + "AC QC KS KD 9D 3C\n"
                     + "9H 5S\n"
+                    + "KS 3C 2D 4D 6D 9D KD Flush : KD\n"
+                    + "7S TS KS KD 9D")]
+        [InlineData("KC 9S KS KD 9D 3C 6D\n"
+                    + "9C AH KS KD 9D 3C 6D\n"
+                    + "AC QC KS KD 9D 3C\n"
+                    + "KH KC KS KD 9D 3C 6D\n"
+                    + "4D 2D KS KD 9D 3C 6D\n"
+                    + "7S TS KS KD 9D",
+                    "3C 6D 9D 9S KC KD KS Full House : K\n"
+                    + "3C 6D 9C 9D KD KS AH Two Pairs : Pair1 of K, Pair2 of 9, A\n"
+                    + "AC QC KS KD 9D 3C\n"
+                    +"3C 6D 9D KC KD KH KS Four Of A Kind : K (Winner)\n"
                     + "KS 3C 2D 4D 6D 9D KD Flush : KD\n"
                     + "7S TS KS KD 9D")]
         public void GeneralTests(string input, string expected)
